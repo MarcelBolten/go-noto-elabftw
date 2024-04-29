09c447726275c65e2703efb43da7bb3e780e016f
Marcel Bolten <github@marcelbolten.de>
2024-04-29 15:24:01 -0700

    make check_coverage.sh less noisy

--- check_coverage.sh
+++ check_coverage.sh
@@ -4,15 +4,18 @@
 # which we have not yet packaged.
 
 url="notofonts.github.io"
-# Capture all lines between "Scripts" and "Noto Dashboard" (exclusive)
+# Use w3m text browser to capture all lines between "Scripts" and "Noto Dashboard" (exclusive)
 all_scripts=$(w3m -dump "$url" | awk '/Scripts/{flag=1; next} /Noto Dashboard/{flag=0} flag')
 
 # Convert multi-line string to array (each line is new member)
 readarray -t scripts <<<"$all_scripts"
 
-for s in "${scripts[@]}"; do
-    # strip spaces, tabs, etc. from "$s" before grepping
-    if ! grep -iq "${s//[[:blank:]]}" categories.sh; then
-        echo "$s from upstream Noto fonts not packaged yet"
+for script in "${scripts[@]}"; do
+    # strip spaces, tabs, etc. and apostrophe (N' Ko script) from "$script" before grepping
+    if ! grep -iq "${script//[[:blank:]\']}" categories.sh; then
+        echo "$script from upstream Noto fonts not packaged yet"
     fi
 done
+
+echo "Latin, Greek, Cyrillic is NotoSans"
+echo "Old Hungarian Ui might be deprecated, see https://github.com/notofonts/old-hungarian-ui/issues/11"
